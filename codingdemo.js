4540553d0aa34db622fc883a8580468699d7bbf4
giovanniASC6 <giovanni.owens@gmail.com>
2019-07-12 13:38:09 -0400

    codingdemo

---
+++ codingdemo.js
@@ -0,0 +1,14 @@
+const rawText = 
+"Judge connected on the first pitch from Ryne Stanek (0-2) to give him nine career multihomer games"
+
+function singleArray(str) {
+    const result = []
+    Let stringLower = str.tolowercase ();
+    for (let i=0; i<str.length; i++) {
+    const char = str[i]
+    if(char = Number) {res = str.replaced(Number, stringLower)}
+    else if( char = stringUpper) {res = str.replaced(stringUpper, stringLower)}
+
+    } 
+    return result
+}
